c39e71a1bafd527241fa07636a96abdf8c9b05d0
Henryatanus <160388388+Henryatanus@users.noreply.github.com>
2024-07-03 16:24:46 +0300

    Add files via upload
    Day one python recess

---
+++ Python_recess_day1.py
@@ -0,0 +1,12 @@
+#Python Basics
+
+
+print('Hello Christ,its me')
+
+fname = input("Enter students name: " )
+
+best_dish = input("Enter best dish: ")
+
+print( "His first name is: " + fname  + " and prefers " + best_dish)
+
+print("End of day one class")
